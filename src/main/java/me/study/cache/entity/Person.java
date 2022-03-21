7b276546937b13054b345ce43f73080e27ca9c95
k-gn <rlarbska97@gmail.com>
2022-03-21 15:02:01 +0900

    config / localdatetime 직렬화 이슈 처리

--- src/main/java/me/study/cache/entity/Person.java
+++ src/main/java/me/study/cache/entity/Person.java
@@ -1,5 +1,9 @@
 package me.study.cache.entity;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.*;
 import org.springframework.data.redis.core.RedisHash;
 
@@ -23,6 +27,9 @@ public class Person implements Serializable {
     private Long id;
     private String name;
     private Integer age;
+
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime createdAt;
 
     @Override
